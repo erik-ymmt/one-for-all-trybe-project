a41c4bd7cd0991a150633e1cb061cca344cb2de9
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-23 19:17:04 -0300

    feat: Insert data into tables

--- desafio1.sql
+++ desafio1.sql
@@ -13,7 +13,7 @@ DROP DATABASE IF EXISTS SpotifyClone;
       username VARCHAR(255) NOT NULL,
       age TINYINT,
       plan_id INT NOT NULL,
-      data_assinatura DATE,
+      subscription_date DATE,
       FOREIGN KEY (plan_id) REFERENCES SpotifyClone.plans (plan_id)
   ) engine = InnoDB;
 
@@ -47,42 +47,84 @@ DROP DATABASE IF EXISTS SpotifyClone;
     CREATE TABLE SpotifyClone.listening_history(
 	  user_id INT NOT NULL,
       song_id INT NOT NULL,
+      listening_date DATE,
       CONSTRAINT PRIMARY KEY (user_id, song_id)
   ) engine = InnoDB;
 
-  INSERT INTO SpotifyClone.plans (coluna1, coluna2)
+  INSERT INTO SpotifyClone.plans (`type`, price)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+    ('gratuito', 0.00),
+    ('familiar', 7.99),
+    ('universitário', 5.99),
+    ('pessoal', 6.99);
 
-  INSERT INTO SpotifyClone.users (coluna1, coluna2)
+  INSERT INTO SpotifyClone.users (username, age, plan_id, subscription_date)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+    ('Barbara Liskov', 82, 1, '2019-10-20'),
+    ('Robert Cecil Martin', 58, 1, '2017-01-06'),
+    ('Ada Lovelace', 37, 2, '2017-12-30'),
+    ('Martin Fowler', 46, 2, '2017-01-17'),
+    ('Sandi Metz', 58, 2, '2018-04-29'),
+    ('Paulo Freire', 19, 3, '2018-02-14'),
+    ('Bell Hooks', 26, 3, '2018-01-05'),
+    ('Christopher Alexander', 85, 4, '2019-06-05'),
+    ('Judith Butler', 45, 4, '2020-05-13'),
+    ('Jorge Amado', 58, 4, '2017-02-17')
+    ;
 
   INSERT INTO SpotifyClone.artists (coluna1, coluna2)
   VALUES
     ('exemplo de dados 1', 'exemplo de dados X'),
     ('exemplo de dados 2', 'exemplo de dados Y');
 
-  INSERT INTO SpotifyClone.artists_followed (coluna1, coluna2)
+  INSERT INTO SpotifyClone.artists_followed (`name`)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+    ('Beyoncé'),
+    ('Queen'),
+    ('Elis Regina'),
+    ('Baco Exu do Blues'),
+    ('Blind Guardian'),
+    ('Nina Simone');
 
-  INSERT INTO SpotifyClone.albums (coluna1, coluna2)
+  INSERT INTO SpotifyClone.albums (title, artist_id, release_year)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+    ('Renaissance', 1, 2022),
+    ('Jazz', 2, 1978),
+    ('Hot Space', 2, 1982),
+    ('Falso Brilhante', 3, 1998),
+    ('Vento de Maio', 3, 2001),
+    ('QVVJFA?', 4, 2003),
+    ('Somewhere Far Beyond', 5, 2007),
+    ('I Put A Spell On You', 6, 2012);
 
-  INSERT INTO SpotifyClone.songs (coluna1, coluna2)
+  INSERT INTO SpotifyClone.songs (song_name, song_duration, album_id)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+    ('Samba em Paris', 267, 6),
+    ('VIRGO’S GROOVE', 369, 1),
+    ('Feeling Good', 100, 8),
+    ('O Medo de Amar é o Medo de Ser Livre', 207, 5),
+    ('Under Pressure', 152, 3),
+    ('BREAK MY SOUL', 279, 1),
+    ('Don’t Stop Me Now', 203, 2),
+    ('The Bard’s Song', 244, 7),
+    ('ALIEN SUPERSTAR', 116, 1),
+    ('Como nossos pais', 105, 4);
 
   INSERT INTO SpotifyClone.listening_history (coluna1, coluna2)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+    (1, 1, "2022-02-28 10:45:55"),
+    (1, 2, "2020-05-02 05:30:35"),
+    (1, 3, "2020-03-06 11:22:33"),
+    (2, 3, "2022-08-05 08:05:17"),
+    (2, 4, "2020-01-02 07:40:33"),
+    (3, 3, "2020-11-13 16:55:13"),
+    (3, 2, "2020-12-05 18:38:30"),
+    (4, 1, "2021-08-15 17:10:10"),
+    (5, 1, "2022-01-09 01:44:33"),
+    (5, 5, "2020-08-06 15:23:43"),
+    (6, 4, "2017-01-24 00:31:17"),
+    (6, 6, "2017-10-12 12:35:20"),
+    (7, 7, "2011-12-15 22:30:49"),
+    (8, 7, "2012-03-17 14:56:41"),
+    (9, 8,"2022-02-24 21:14:22"),
+    (10, 9, "2015-12-13 08:30:22");
